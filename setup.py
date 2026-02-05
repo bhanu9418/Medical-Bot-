3bf6a5f4ea54f173339a65c84737acb3c20d2690
Bhanu9418 <bhanuprakash9418@gmail.com>
2026-02-05 16:29:00 +0530

    completed

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name = "mediacal_chatbot",
+    version="0.1.0",
+    author =" I AM",
+    author_email= " bhanuprakash9418@gmail.com",
+    packages= find_packages(),
+    install_requries=[]
+)
+
+
